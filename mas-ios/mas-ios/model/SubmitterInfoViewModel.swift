95ba4327dab7fd1438858a7806df433240b1dd1f
Jihong <jihong2253@gmail.com>
2024-01-20 18:10:19 -0500

    add submitter info view model

--- mas-ios/mas-ios/model/SubmitterInfoViewModel.swift
+++ mas-ios/mas-ios/model/SubmitterInfoViewModel.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+import SwiftUI
+
+
+class SubmitterInfoViewModel: ObservableObject {
+    
+    @Published var submitterName: String
+    @Published var submitterPhone: String
+    @Published var submitterEmail: String
+    @Published var alertMessage: String = ""
+    @Published var showAlert = false
+    @Published var isEmailValid = false
+    @Published var isPhoneValid = false
+    @Published var inProgress = false
+    
+    init() {
+        self.submitterName = ""
+        self.submitterPhone = ""
+        self.submitterEmail = ""
+    }
+    
+    func submitInfo() {
+        let userInfo = UserInfo(submitterName: self.submitterName,
+                                submitterPhone: self.submitterPhone,
+                                submitterEmail: self.submitterEmail)
+        
+        guard let uploadData = try? JSONEncoder().encode(userInfo) else {
+            return
+        }
+    }
+}
