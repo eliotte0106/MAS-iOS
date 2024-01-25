1584cf99ad08d881ac7e26126eeb7f6b13b38690
Jihong <jihong2253@gmail.com>
2024-01-24 20:59:05 -0500

    add terms/cond

--- mas-ios/mas-ios/views/TermsAndConditionsView.swift
+++ mas-ios/mas-ios/views/TermsAndConditionsView.swift
@@ -6,3 +6,46 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct TermsAndConditionsView: View {
+    
+    @Binding var agreeToTerms: Bool
+    @Binding var isPresented: Bool
+    
+    var body: some View {
+        
+        VStack {
+            ScrollView {
+                Text("This is for MAS course.")
+            }.padding(12)
+            
+            Button {
+                agreeToTerms = true
+                isPresented = false
+            } label: {
+                Text("Agree")
+                    .frame(width: 300, height: 40)
+                    .foregroundColor(.white)
+                    .background(.blue)
+                    .cornerRadius(6)
+            }
+            Button {
+                agreeToTerms = false
+                isPresented = false
+            } label: {
+                Text("Disagree")
+                    .frame(width: 300, height: 40)
+                    .foregroundColor(.blue)
+                    .cornerRadius(6)
+            }
+        }
+
+    }
+}
+
+struct TermsAndConditionsView_Previews: PreviewProvider {
+    static var previews: some View {
+        TermsAndConditionsView(agreeToTerms: .constant(false), isPresented: .constant(true))
+    }
+}
