5cd121627d802a33d5bc7b8d83f18fd2dd45628b
Jihong <jihong2253@gmail.com>
2024-01-25 21:58:02 -0500

    edit submitter info view

--- mas-ios/mas-ios/views/SubmitterInfoView.swift
+++ mas-ios/mas-ios/views/SubmitterInfoView.swift
@@ -10,15 +10,18 @@ import iPhoneNumberField
 
 struct SubmitterInfoView: View {
     @Environment(\.dismiss) private var dismiss
-    
     @StateObject var viewModel = SubmitterInfoViewModel()
-    
+    @StateObject var locationManager = LocationManager()
     @State var isPresented = false
     
-
+    private var _carImage: CarImage
+    init(carImage: CarImage) {
+        self._carImage = carImage
+    }
     
     var body: some View {
         VStack {
+            
             Form {
                 TextField("Name", text: $viewModel.submitterName, prompt: Text("First Last").italic())
                 
@@ -28,6 +31,10 @@ struct SubmitterInfoView: View {
                         .padding(.top, 5)
                 }
                 
+                iPhoneNumberField("Phone",text: $viewModel.submitterPhone)
+                    .maximumDigits(10)
+                
+                
                 if viewModel.validationErrors.contains(.invalidPhone) {
                     Text("Please enter a valid 10 digit phone")
                         .foregroundColor(.red)
@@ -35,19 +42,71 @@ struct SubmitterInfoView: View {
                 }
                 
                 TextField("Email", text: $viewModel.submitterEmail,
-                          prompt: Text("pjh@email.com")
+                          prompt: Text("jihong@email.com")
                     .italic())
                 .textInputAutocapitalization(.never)
                 
                 if viewModel.validationErrors.contains(.invalidEmail) {
-                    Text("Please enter a correct from of email")
+                    Text("Please enter a valid from of an email")
+                        .foregroundColor(.red)
+                        .padding(.top, 5)
+                }
+                
+                HStack {
+                    Text("I Accept")
+                    Button {
+                        isPresented = true
+                    } label: {
+                        Text(" this is only for MAS course")
+                    }
+                    Spacer()
+                    viewModel.agreeToTerms ? Image(systemName: "checkmark")
+                        .foregroundColor(.green) : nil
+                }
+                
+                if viewModel.validationErrors.contains(.invalidTac) {
+                    Text("You must accept terms and conditions that this is for MAS course")
                         .foregroundColor(.red)
                         .padding(.top, 5)
                 }
             }
             .disableAutocorrection(true)
+            
+            Group {
+                if viewModel.inProgress {
+                    HStack {
+                        Text("Submitting request...")
+                            .font(.headline)
+                        ProgressView()
+                    }
+                }
+                else {
+                    Button {
+                        guard viewModel.validateInfo() else { return }
+                        
+                        viewModel.submitInfo(self._carImage, self.locationManager.latLong())
+                    } label: {
+                        
+                        Text("Submit")
+                            .frame(width: 300, height: 40)
+                            .foregroundColor(.white)
+                            .background(.blue)
+                            .cornerRadius(6)
+                    }
+                }
+            }
+            
+            
             Spacer()
         }
+        .alert(isPresented: $viewModel.showAlert) {
+            Alert(title: Text(viewModel.alertType.title),
+                  message: Text(viewModel.alertMessage),
+                  dismissButton: .default(Text("OK")))
+        }
+        .sheet(isPresented: $isPresented, content: {
+            TermsAndConditionsView(agreeToTerms: $viewModel.agreeToTerms, isPresented: $isPresented)
+        })
         .navigationTitle("User info")
         .navigationBarBackButtonHidden(true)
         .navigationBarTitleDisplayMode(.inline)
@@ -63,3 +122,8 @@ struct SubmitterInfoView: View {
     }
 }
 
+struct SubmitterInfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        SubmitterInfoView(carImage: CarImage(image: Image("test"), uiImage: UIImage()))
+    }
+}
